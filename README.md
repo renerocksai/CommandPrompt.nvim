ffeaa7afabb9532711cc65b67a6028ff58be5c6b
Rene Schallner <rene@renerocks.ai>
2023-04-01 03:21:36 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# CommandPromt.nvim - Neovim Plugin for a simple command picker
+
+This is a plugin for Neovim that allows you to easily pick and execute commands
+using Telescope.
+
+## Installation
+
+To use this plugin, you need to have
+[Telescope](https://github.com/nvim-telescope/telescope.nvim) installed.
+
+You can install this plugin using your favorite plugin manager. For example,
+using [vim-plug](https://github.com/junegunn/vim-plug):
+
+```vim
+Plug 'renerocksai/commandprompt'
+```
+
+## Usage
+
+This plugin defines a list of default commands that you can execute using the
+command picker. You can also define your own commands by passing them to the
+`setup` function:
+
+
+```lua
+require('commandprompt').setup({
+  {
+    description = "Summarize",
+    cmd = ':ChatGPTRun turbo-sum',
+  }
+  ,{
+    description = "Make it sound more professional",
+    cmd = ':ChatGPTRun en-professional',
+  }
+})
+-- Add a keymapping to open the command picker
+vim.api.nvim_set_keymap('n', '<Leader>g', ':lua require("gptcommands").command_picker()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<Leader>g', ':lua require("gptcommands").command_picker()<CR>', { noremap = true, silent = true })
+```
+
+To open the command picker, press `<Leader>g` in normal mode or `<Leader>g` in
+visual mode.
+
+## Customization
+
+You can customize the behavior of the command picker by passing options to the
+`command_picker` function. For example:
+
+```lua
+require('gptcommands').command_picker({
+  prompt_title = "My Commands",
+  sorter = require('telescope.sorters').get_fzy_sorter(),
+})
+```
+
+For more information on the available options, see the [Telescope documentation](https://github.com/nvim-telescope/telescope.nvim#pickers).
+
+## License
+
+This plugin is licensed under the MIT License. See the [LICENSE](LICENSE) file
+for details.
+
---
+++ lua/commandprompt.lua
@@ -0,0 +1,83 @@
+-- Neovim Plugin for command picker
+
+-- Define the default commands list
+local default_commands = {
+  {
+    description = "Search for pattern",
+    cmd = ':vimgrep /%s/ **/*',
+    args = {"pattern"}
+  },
+  {
+    description = "Show Git log for file",
+    cmd = ':Git log -- %s',
+    args = {"file"}
+  },
+}
+
+-- Create the Telescope picker
+local telescope = require('telescope')
+local actions = require('telescope.actions')
+local action_state = require("telescope.actions.state")
+local sorters = require('telescope.sorters')
+local pickers = require('telescope.pickers')
+local finders = require('telescope.finders')
+
+local make_command_entry = function(command)
+  local args = ""
+  if command.args then
+    args = " " .. table.concat(command.args, " ")
+  end
+
+  return {
+    value = command.cmd,
+    display = command.description .. " (" .. command.cmd .. args .. ")",
+    ordinal = command.description,
+    cmd = command.cmd,
+    args = command.args,
+  }
+end
+
+local command_picker = function(opts)
+  pickers.new(opts, {
+    prompt_title = "Commands",
+    finder = finders.new_table({
+      results = M.commands,
+      entry_maker = make_command_entry,
+    }),
+    sorter = sorters.get_generic_fuzzy_sorter(),
+    attach_mappings = function(prompt_bufnr, map)
+        actions.select_default:replace(function()
+            actions.close(prompt_bufnr)
+            local selection = action_state.get_selected_entry()
+            local args = {}
+            for _, arg in ipairs(selection.args or {}) do
+              local input = vim.fn.inputdialog(arg .. ":")
+              if input ~= "" then
+                table.insert(args, input)
+              end
+            end
+            local command = string.format(selection.cmd, unpack(args))
+            vim.cmd(command)
+        end)
+        return true
+    end,
+  }):find()
+end
+
+
+function M.setup(user_commands)
+  -- Override the default commands list with user-provided commands
+  user_commands = user_commands or default_commands
+  if user_commands then
+    M.commands = user_commands
+  end
+
+end
+
+function M.command_picker()
+  -- Open the command picker
+  command_picker({})
+end
+
+return M
+
